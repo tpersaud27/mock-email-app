425fb02c924fd0ac7e0bad85f16babd00e86c168
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2023-12-13 20:29:00 +0100

    Setting up routing

--- mock-email-client/src/app/auth/auth-routing.module.ts
+++ mock-email-client/src/app/auth/auth-routing.module.ts
@@ -1,10 +1,15 @@
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- mock-email-client/src/app/auth/auth.module.ts
+++ mock-email-client/src/app/auth/auth.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    SignInComponent,
-    SignUpComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [SignInComponent, SignUpComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
-export class AuthModule { }
+export class AuthModule {}
