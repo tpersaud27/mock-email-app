3017a1f46f36dc1636b8ad62d71c568df1778b9c
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2023-12-14 18:38:08 +0100

    nav header markup

--- mock-email-client/src/app/app.module.ts
+++ mock-email-client/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthRoutingModule } from './auth/auth-routing.module';
 import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- mock-email-client/src/app/shared/shared.module.ts
+++ mock-email-client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, NavHeaderComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [InputComponent],
+  exports: [InputComponent, NavHeaderComponent],
 })
 export class SharedModule {}
