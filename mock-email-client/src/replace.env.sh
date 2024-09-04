a7d6d0f9eeadb454ab5a9424476d9c88a77cbc0c
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2024-09-04 13:30:24 -0400

    Update script

--- mock-email-client/src/replace.env.sh
+++ mock-email-client/src/replace.env.sh
@@ -3,13 +3,7 @@
 # Replace display version environment variable
 echo "DISPLAY_VERSION is: $DISPLAY_VERSION"
 
-# Go to file location
-# cd src/environments
-
-# Print current directory
-echo "Current directory: $(pwd)"
-
 # Replace version
-sed -i "s/\${DISPLAY_VERSION}/${DISPLAY_VERSION}/g" src/environmentsenvironment.ts
+sed -i "s/\${DISPLAY_VERSION}/${DISPLAY_VERSION}/g" src/environments/environment.ts
 # Replace title
 sed -i "s/\${TITLE}/${DISPLAY_VERSION}/g" src/environments/environment.ts
