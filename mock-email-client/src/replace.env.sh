b6eff2d14abb6421f3d19fe7c0320eed6342ce12
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2024-09-04 13:26:42 -0400

    Move GHA to script

--- mock-email-client/src/replace.env.sh
+++ mock-email-client/src/replace.env.sh
@@ -4,9 +4,12 @@
 echo "DISPLAY_VERSION is: $DISPLAY_VERSION"
 
 # Go to file location
-cd src/environments
+# cd src/environments
 
 # Print current directory
 echo "Current directory: $(pwd)"
 
-sed -i "s/\${DISPLAY_VERSION}/${DISPLAY_VERSION}/g" environment.ts
+# Replace version
+sed -i "s/\${DISPLAY_VERSION}/${DISPLAY_VERSION}/g" src/environmentsenvironment.ts
+# Replace title
+sed -i "s/\${TITLE}/${DISPLAY_VERSION}/g" src/environments/environment.ts
