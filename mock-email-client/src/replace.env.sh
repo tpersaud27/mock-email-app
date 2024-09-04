f2ac5bf3ae2dc1fdf233fd77eccffbede6bd76ea
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2024-09-04 13:13:33 -0400

    update

--- mock-email-client/src/replace.env.sh
+++ mock-email-client/src/replace.env.sh
@@ -10,7 +10,7 @@ echo "Current date and time: $(date)"
 # Replace display version environment variable
 echo "DISPLAY_VERSION is: $DISPLAY_VERSION"
 # Go to file location
-cd ./environments
+cd src/environments
 # Print current directory
 echo "Current directory: $(pwd)"
 sed -i "s|\${DISPLAY_VERSION}|${{ DISPLAY_VERSION }}|g" environment.ts
